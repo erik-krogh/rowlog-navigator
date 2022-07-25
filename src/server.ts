3ed5606204bff509719ecc1f67e17ef61792ab7a
Erik Krogh Kristensen <erik-krogh@github.com>
2022-07-25 20:51:09 +0200

    make a server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+// a server that constantly refreshes the cache of activities.
+import express from "express";
+
+import * as eventFetcher from "./api/eventFetcher";
+
+setInterval(() => {
+  console.log("Refreshing cache...");
+  eventFetcher.saveCurrentEvents();
+}, 60 * 60 * 1000);
+
+// start the server
+const app = express();
+app.get("/", async (req, res) => {
+  const events = await eventFetcher.events();
+  res.status(200).json(events);
+});
+
+// start the server
+const port = process.env.PORT || 9001;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
